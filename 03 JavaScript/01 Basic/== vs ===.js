055b656afabca05d3ed9cfc5df1629e9bfeb9e88
yash jivani <yashjivani17@gmail.com>
2022-08-08 21:21:32 +0530

    == vs ===

---
+++ 03 JavaScript/01 Basic/== vs ===.js
@@ -0,0 +1,20 @@
+/*
+
+Equality Operator
+
+==  : compare without data type 
+=== : compare with data type (strict comparison)
+!=  : compare without data type 
+!== : compare with data type (strict comparison)
+
+*/
+
+console.log(5==5);
+console.log(5=='5');
+console.log(5==='5');
+console.log(5===5);
+
+console.log(5!=5);
+console.log(5!==5);
+console.log(5!=='5');
+console.log(5!='5');
