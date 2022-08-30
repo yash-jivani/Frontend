9d14f84fac133330edfc769011e936017d238422
yash jivani <yashjivani17@gmail.com>
2022-08-30 14:15:07 +0530

    Dates

---
+++ 03 JavaScript/08 Numbers, Dates, IntI & Timers/Dates.js
@@ -0,0 +1,29 @@
+const future = new Date(2037, 10, 19, 15, 23);  // year, month, date, hour, mint
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142256980000));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
+
+// Operations With Dates
+const future2 = new Date(2037, 10, 19, 15, 23);
+console.log(future2);
+console.log(+future2);
+
+const calcDaysPassed = (date1, date2) =>
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
+console.log(days1);
