b7ba71cf41babe3ab732729c12380caafec2be70
yash jivani <yashjivani17@gmail.com>
2022-08-31 09:33:36 +0530

    init assets

---
+++ 04 Mini Projects I/Bankist 2.0/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+///////////////////////////////////////
+// Modal window
+
+const overlay = document.querySelector(".overlay");
+const modal = document.querySelector(".modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnsOpenModal.forEach((btn) => {
+  return btn.addEventListener("click", openModal);
+});
+
+btnCloseModal.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
