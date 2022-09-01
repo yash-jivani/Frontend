9ea926d7eb5ba4402daa69d6d705bf9025d5d1e4
yash jivani <yashjivani17@gmail.com>
2022-09-01 08:50:10 +0530

    page navigation

--- 04 Mini Projects I/Bankist 2.0/script.js
+++ 04 Mini Projects I/Bankist 2.0/script.js
@@ -1,12 +1,13 @@
 "use strict";
 
-///////////////////////////////////////
-// Modal window
-
+const mainNav = document.querySelector(".nav__links");
 const overlay = document.querySelector(".overlay");
 const modal = document.querySelector(".modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnCloseModal = document.querySelector(".btn--close-modal");
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -28,5 +29,27 @@ btnCloseModal.addEventListener("click", closeModal);
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
+  }
+});
+
+///////////////////////////////////////
+// Page Navigation
+
+// const navLinks = document.querySelectorAll(".nav__link");
+// navLinks.forEach((currentLink) => {
+//   currentLink.addEventListener("click", function (e) {
+//     e.preventDefault();
+
+//     const id = currentLink.getAttribute("href");
+//     // console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+
+mainNav.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
