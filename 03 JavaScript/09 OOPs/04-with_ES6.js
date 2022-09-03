46c7e1d633baa2ce195209a2853c5e2b587a8201
yash jivani <yashjivani17@gmail.com>
2022-09-03 16:43:14 +0530

    ES6 classes

---
+++ 03 JavaScript/09 OOPs/04-with_ES6.js
@@ -0,0 +1,39 @@
+///////////////////////////////////////
+// ES6 Classes
+// ! 1. Classes are NOT hoisted
+// ! 2. Classes are first-class citizens
+// ! 3. Classes are executed in strict mode
+
+// * Class expression
+// const PersonCl = class {}
+
+// * Class declaration
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Instance methods
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+
+}
+
+const jessica = new PersonCl("Jessica Davis", 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+jessica.greet();
+
