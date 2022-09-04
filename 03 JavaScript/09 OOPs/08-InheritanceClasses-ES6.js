ef78da4b28909f92dee2d59fdd99de8b2145e0dd
yash jivani <yashjivani17@gmail.com>
2022-09-04 12:47:46 +0530

    Inheritance Classes with ES6

---
+++ 03 JavaScript/09 OOPs/08-InheritanceClasses-ES6.js
@@ -0,0 +1,56 @@
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Instance methods
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  // Getters & Setters
+  get getName() {
+    return this.fullName;
+  }
+
+  set changeName(newName) {
+    this.fullName = newName;
+  }
+  // Static method
+  static hey() {
+    console.log("Hey there 👋");
+  }
+}
+
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    // Always needs to happen first!
+    super(fullName, birthYear);
+    this.course = course;
+  }
+
+  introduce() {
+    console.log(`My name is ${this.fullName} and I study ${this.course}`);
+  }
+
+  calcAge() {
+    console.log(
+      `I'm ${
+        2037 - this.birthYear
+      } years old, but as a student I feel more like ${
+        2037 - this.birthYear + 10
+      }`
+    );
+  }
+}
+
+const martha = new StudentCl("Martha Jones", 2012, "Computer Science");
+martha.introduce();
+martha.calcAge();
+StudentCl.hey();
+console.log(martha.getName);
