721c15931881ef01a7574323d818d122d4eca1b0
yash jivani <yashjivani17@gmail.com>
2022-09-05 09:16:19 +0530

    Init assets

---
+++ 04 Mini Projects I/Mapty/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const containerWorkouts = document.querySelector(".workouts");
+const form = document.querySelector(".form");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
