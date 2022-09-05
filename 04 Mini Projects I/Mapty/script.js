7c6ec240de8ca15c6994a53c706685294efbf762
yash jivani <yashjivani17@gmail.com>
2022-09-05 10:05:13 +0530

    Display Map

--- 04 Mini Projects I/Mapty/script.js
+++ 04 Mini Projects I/Mapty/script.js
@@ -10,3 +10,35 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+
+// * Geolocation API
+if (navigator.geolocation) {
+  // 1st callback => for success (getting current position)
+  // 2nd callback => for failure (getting current position)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      //   console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      //   console.log(latitude, longitude);
+      //   const url = `https://www.google.com/maps/@${latitude},${longitude}`;
+      //   console.log(url);
+
+      const coords = [latitude, longitude];
+      // -- Map --
+      const map = L.map("map").setView(coords, 13); // 13 => zoom lev.
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+        .openPopup();
+    },
+    function () {
+      alert("Could not get your position");
+    }
+  );
+}
